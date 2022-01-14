4a2990788c829148ea715c9f0f050255352b715a
davis <zanxiaofeng@163.com>
2022-01-14 11:24:13 +0800

    update

--- linux-io/file-io/file-create.c
+++ linux-io/file-io/file-create.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 	}
 
 
-	int fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0664);
+	int fd = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0777);
 	if (fd == -1){
 		printf("Error when create a file\n");
 		return 1;
